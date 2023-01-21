72207d53452673eae3fc320351ad54eee6ef97f3
Moliki-Salman <salmanameenat@gmail.com>
2023-01-21 12:11:15 +0100

    intro-to-C

---
+++ intro-to-C.c
@@ -0,0 +1,14 @@
+/* - C is a compiled language. To run C, you need to first compile it */
+/* - A function is a routine that takes in one or more arguments and returns a single value.*/
+/* -The stdio is a libraray that provides the printf().
+- the function is wrapped into a main() function.
+-  */
+/* -the int keyword is a return value
+  - main(void) means that the function main() will have no argument/parameters, using the keyword void.
+- the function has a body which is opend by a curly braces{}, inside the {} we have all codes needed to perform its operation. */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("Hello, world");
+}
