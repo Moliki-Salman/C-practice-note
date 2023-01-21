f060a8ad5639118a6eaf777c62593587d50c6c86
Moliki-Salman <salmanameenat@gmail.com>
2023-01-21 21:57:26 +0100

    constant

---
+++ constants.c
@@ -0,0 +1,10 @@
+/* - Upper case name means constant while variable  is declared usong lowercase in C eg*/
+const int NUM = 29;
+const int AGE = 30;
+/* you can also define your const is way:*/
+#define SONG 23
+/* here you do not need to add a type,or the equal sign =, and semicolon. 
+the C compiler will infer the type from thr value specified at compile time.
+MEANING THAT ALL THE PROTOTYPES IN ALX ARE CONST */
+#ifndef MAIN_h
+/* #ifndef is used to recall a already defined code. it is also used to apply an already defined code multiple times  without the result from the initial application.   */
